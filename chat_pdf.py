bf605855ab2f2dcd16fa2ba2f2f08c6708c23442
abubakar5081 <imaubakar508@gmail.com>
2024-04-28 15:18:43 +0300

    fix(typo): fixed a typo error on the providers

--- chat_pdf.py
+++ chat_pdf.py
@@ -8,7 +8,7 @@ def embedchain_bot(db_path, api_key):
     return App.from_config(
         config={
             "llm": {"provider": "openai", "config": {"api_key": api_key}},
-            "vectordb": {"prvoider": "chroma", "config": {"dir": db_path}},
+            "vectordb": {"provider": "chroma", "config": {"dir": db_path}},
             "embedder": { "provider": "openai", "config": {"api_key": api_key}},
         }
     )
